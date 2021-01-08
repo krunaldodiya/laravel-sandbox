65a372fc9823dc502308b4c39e633f8ce3ff6cb3
Krunal Dodiya <kunal.dodiya1@gmail.com>
2021-01-08 10:50:25 +0530

    init

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', function () {
-    return response(['message'=> "hello there"],200);
+    return response(['message' => "hello krunal"], 200);
 });
